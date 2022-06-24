e5d5d1ec5d1837d57644fdfb6969cbe59ca6d617
Rosma Khoirul Asiyya <rosmaasiyya8@gmail.com>
2022-06-24 20:12:03 +0700

    update terbaru
    login, register, landing page, login vendor (tadi gk bisa commit)

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <script
-      src="https://kit.fontawesome.com/64d58efce2.js"
-      crossorigin="anonymous"
-    ></script> -->
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous">
+    </script>
     <title>Venika</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="frontend/css/style.css">
 </head>
 <body>
     <div class="container">
         <div class="forms-container">
             <div class="signin-signup">
-                <form action="login.php" method="POST" class="sign-in-form">
+                <form action="#" class="sign-in-form">
+                    
                     <h2 class="title">Login</h2>
                     
                     <div class="input-field">
@@ -32,24 +32,20 @@
                     </div>
 
                     <p class="lupa_pass">Lupa Password ?</p>
-                    <input type="submit" name="login" value="login" class="btn solid">
-
-                    <!-- <p class="social_text">Or Sign in with social media</p>
-                    <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                    </div> -->
+                    
+                    <input type="submit" value="Login" class="btn solid">
+                    
                 </form>
 
                 <form action="register.php" method="POST" class="sign-up-form">
                     <h2 class="title">Register</h2>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
-                        <input type="text" name="reg_user" placeholder="Username">
+                        <input type="text" placeholder="Nama Lengkap">
+                    </div>
+                    <div class="input-field">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Username">
                     </div>
                     <div class="input-field">
                         <i class="fas fa-envelope"></i>
@@ -59,17 +55,11 @@
                         <i class="fas fa-lock"></i>
                         <input type="password" name="reg_pass" placeholder="Password">
                     </div>
-                    <input type="submit" name="register" value="register" class="btn solid">
-
-                    <!-- <p class="social_text">Or Sign up with social media</p>
-                    <div class="social-media">
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" class="social-icon">
-                            <i class="fab fa-google"></i>
-                        </a>
-                    </div> -->
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Konfirmasi Password">
+                    </div>
+                    <input type="submit" value="Register" class="btn solid">
 
                     </form>
                 </div>
@@ -81,6 +71,11 @@
                         <h3>Belum Punya Akun ?</h3>
                         <p>Cari kebutuhan pernikahan lebih mudah dan cepat dengan venika</p>
                      <button class="btn transparent" id="sign-up-btn">Register</button>
+
+                     <p>
+                         Ingin Daftar sebagai vendor ? 
+                         <a href="" class="daftar_vendor"> Daftar Vendor</a>
+                    </p>
                     </div>
 
                     <img src="img/illustrasi_login.png" class="image" alt="">
@@ -88,7 +83,7 @@
                 <div class="panel right-panel">
                     <div class="content">
                         <h3>Sudah Punya Akun ?</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>Eksplore berbagai vendor pernikahan terbaik melalui Venika..</p>
                         <button class="btn transparent" id="sign-in-btn">Login</button>
                     </div>
                     <img src="img/illustrasi_register.png" class="image" alt="">
